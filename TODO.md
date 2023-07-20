70fada5614c361a04df3fd645d7f86335a0adbca
ganynmednet <ganymednet@gmail.com>
2023-07-20 21:30:53 +0100

    # 20/07 + Idea & Features https://docs.google.com/spreadsheets/d/1yclw_rW2CbmHPeE29W4cb_M0JjJRtiE7CBNqzAfWZt8/edit#gid=0

--- TODO.md
+++ TODO.md
@@ -1,5 +1,17 @@
 
+# Research
 
-- Idea & Features https://docs.google.com/spreadsheets/d/1yclw_rW2CbmHPeE29W4cb_M0JjJRtiE7CBNqzAfWZt8/edit#gid=0
+
+
+# 21/07
+
+- https://www.producthunt.com/
+- https://respell.ai/
+- https://www.belva.ai/
+- https://www.beebee.com/
+
+
+# 20/07
++ Idea & Features https://docs.google.com/spreadsheets/d/1yclw_rW2CbmHPeE29W4cb_M0JjJRtiE7CBNqzAfWZt8/edit#gid=0
 - Implementation stages
 - First stage plan
---
+++ src/run.py
@@ -0,0 +1,31 @@
+
+
+if __name__ == "__main__":
+
+    """
+    TASK
+      create
+      
+    SEARCH
+        fetch news
+        check for matches each article
+            if match:
+                if exist in the LIST:
+                    get current desc bullet points
+                        if new info:
+                            add to company info
+                            save
+                else:
+                    add company to LIST:
+                      add meta data
+                      decompose to bullet points
+                      save
+        
+    
+    - LIST
+      - Design structure
+    
+    - COMPANY
+      - design structure
+    
+    """
